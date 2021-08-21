58de7f7df18ae9ed65d5c4885c02db0b432f7773
issei morita <isseimorita1@gmail.com>
2021-08-21 20:36:51 +0900

    abc214
    Signed-off-by: issei morita <isseimorita1@gmail.com>

---
+++ abc214/A/main.nim
@@ -0,0 +1,48 @@
+import sequtils,strutils,sugar, algorithm, tables
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc getchar(): char {.header: "<stdio.h>", varargs.}
+proc nextInt(): int = scanf("%lld",addr result)
+proc nextFloat(): float = scanf("%lf",addr result)
+proc nextString(): string =
+  var get = false
+  result = ""
+  while true:
+    var c = getchar()
+    if int(c) > int(' '):
+      get = true
+      result.add(c)
+    else:
+      if get: break
+      get = false
+template cfor(init, comp, incr, body: untyped) =
+  block:
+    init
+    while comp:
+      body
+      incr
+template `max=`*(x,y:typed):void = x = max(x,y)
+template `min=`*(x,y:typed):void = x = min(x,y)
+import atcoder/extra/other/warlus_operator
+
+template times(n: int, body: untyped) =
+  for _ in 0..<n:
+    body
+
+proc `$` [T](x: seq[T]): string = x.mapIt($it).join(" ")
+proc `ceilDiv`[T: SomeInteger](x, y: T): T = x div y + ord(x mod y != 0)
+
+
+proc solve(N:int):auto =
+  if N in 1..125:
+    return 4
+  if N in 126..211:
+    return 6
+  if N in 212..214:
+    return 8
+
+proc main():void =
+  var N = nextInt()
+  echo solve(N)
+  return
+
+main()
---
+++ abc214/D/main.nim
@@ -0,0 +1,50 @@
+import sequtils,strutils,sugar, algorithm, tables
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc getchar(): char {.header: "<stdio.h>", varargs.}
+proc nextInt(): int = scanf("%lld",addr result)
+proc nextFloat(): float = scanf("%lf",addr result)
+proc nextString(): string =
+  var get = false
+  result = ""
+  while true:
+    var c = getchar()
+    if int(c) > int(' '):
+      get = true
+      result.add(c)
+    else:
+      if get: break
+      get = false
+template cfor(init, comp, incr, body: untyped) =
+  block:
+    init
+    while comp:
+      body
+      incr
+template `max=`*(x,y:typed):void = x = max(x,y)
+template `min=`*(x,y:typed):void = x = min(x,y)
+import atcoder/extra/other/warlus_operator
+
+template times(n: int, body: untyped) =
+  for _ in 0..<n:
+    body
+
+proc `$` [T](x: seq[T]): string = x.mapIt($it).join(" ")
+proc `ceilDiv`[T: SomeInteger](x, y: T): T = x div y + ord(x mod y != 0)
+
+
+proc solve(N:int, u:seq[int], v:seq[int], w:seq[int]):string =
+  discard
+
+proc main():void =
+  var N = nextInt()
+  var u = newSeqWith(N-1, 0)
+  var v = newSeqWith(N-1, 0)
+  var w = newSeqWith(N-1, 0)
+  for i in 0..<N-1:
+    u[i] = nextInt()
+    v[i] = nextInt()
+    w[i] = nextInt()
+  echo solve(N, u, v, w)
+  return
+
+main()
